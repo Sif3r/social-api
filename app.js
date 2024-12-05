554c4e3828bab8b6a38c8bd8d5948b0e31989970
Aurélien Aoustin <62019680+Sif3r@users.noreply.github.com>
2024-12-05 16:22:48 +0100

    swagger

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const express = require('express')
 require('dotenv').config();
 const logger = require('morgan')
 
 const app = express()
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'My API',
+      version: '1.0.0',
+      description: 'API documentation',
+    },
+    servers: [
+      { url: 'https://social-api-cvd9b5dmbmacfrcu.francecentral-01.azurewebsites.net/' },
+    ],
+  },
+  apis: ['./routes/*.js'], // Path to your route files
+};
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
 
 app.use(express.json());
 app.use(logger('dev'))
@@ -14,6 +34,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+const fs = require('fs');
+fs.writeFileSync('./swagger.json', JSON.stringify(swaggerDocs));
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/login', require('./routes/auth'));
 app.use('/user', require('./routes/user'))
 app.use('/post', require('./routes/post'))
